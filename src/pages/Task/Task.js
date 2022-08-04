168968831f82a60de3c4fc9f5ea6b848b3e5e4cb
satokoki <sato.k@impl.co.jp>
2022-08-05 08:38:48 +0900

    タブ機能の追加

--- src/pages/Task/Task.js
+++ src/pages/Task/Task.js
@@ -1,6 +1,6 @@
 import { useRef, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
-import "./App.css";
+import "../../App.css";
 import TodoList from "./TodoList";
 
 const Task = () => {
